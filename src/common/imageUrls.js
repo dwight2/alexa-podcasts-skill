f0964f3cabda117f93b5769773cffd42a2abd81a
tomrf1 <tom.forbes@theguardian.com>
2017-02-02 16:35:25 +0000

    Send card with list of podcasts

---
+++ src/common/imageUrls.js
@@ -0,0 +1,4 @@
+exports.cardImages = {
+  'smallImageUrl': 'https://s3.amazonaws.com/alexa-config/images/alexa-small.png',
+  'largeImageUrl': 'https://s3.amazonaws.com/alexa-config/images/alexa-large.png'
+}
--- src/intentHandlers/listPodcasts.js
+++ src/intentHandlers/listPodcasts.js
@@ -1,9 +1,30 @@
+var cardImages = require('../common/imageUrls').cardImages
+
+var podcastsCard = `You can listen to the following podcasts:
+____________________
+Lifestyle & Culture
+Books podcast, Close Encounters, Film Weekly, Music Weekly, The Citadel
+____________________
+News & opinion
+Behind the lines, Politics for humans, Politics Weekly, What would a feminist do?
+____________________
+Science & technology
+Brain waves, Chips with everything, Science Weekly
+____________________
+Sport
+Football Weekly, Premier League: the view from Australia
+____________________
+Storytelling
+The Long Read, The Story
+`
 
 module.exports = function() {
-  //TODO - send card
   this.emit(
-    ':ask',
+    ':askWithCard',
     `I have regular podcasts about football, politics, science and books. You can also listen to in-depth reporting from the Guardian Long Read series. I have sent a full list to your Alexa app. What would you like to hear?`,
-    'How can I help?'
+    'How can I help?',
+    'Podcasts',
+    podcastsCard,
+    cardImages
   )
 }
